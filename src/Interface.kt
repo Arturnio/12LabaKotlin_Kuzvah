2fc1358884948f08fdc8132a195ed1f069687e36
Artur <only.up.ak@gmail.com>
2026-01-18 18:17:21 +0300

    Add interface

---
+++ src/Interface.kt
@@ -0,0 +1,82 @@
+interface Movable{
+    var speed: Int
+    val model: String
+    val number: String
+    fun move()
+    fun stop(){
+        println("Останавливаемся...")
+    }
+}
+class Car(    override val model: String,
+              override val number: String): Movable{
+
+    override var speed = 60
+    override fun move() {
+        println("ЕДем на машине со скоростью $speed км.ч")
+    }
+}
+class Aircraft(override val model: String,
+               override val number: String): Movable{
+    override var speed = 600
+    override fun move() {
+        println("Летим на самолете со скоростью $speed км.ч")
+    }
+    override fun stop() = println("Приземляемся")
+
+}
+
+
+
+fun travel(obj: Movable) = obj.move()
+
+
+
+interface Worker{
+    fun work()
+}
+interface Student{
+    fun study()
+}
+
+
+class WorkingStudent(val name: String): Worker, Student{
+    override fun work() = println("$name working")
+    override fun study() = println("$name study")
+}
+
+
+
+interface VideoPlayable{
+    fun play() = println("Play video")
+}
+
+
+
+interface AudioPlayable{
+    fun play() = println("Play audio")
+}
+
+
+class MediaPlayer: VideoPlayable, AudioPlayable{
+    override fun play(){
+        println("Start playing")
+        super<VideoPlayable>.play()
+        super<AudioPlayable>.play()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+fun main(){
+    val player = MediaPlayer()
+    player.play()
+}
